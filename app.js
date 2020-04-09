43f87a6ef2167b78d4207223a8c9a1119ad1fbb8
ritikgupta21092000 <55480460+ritikgupta21092000@users.noreply.github.com>
2020-04-10 01:10:50 +0530

    Add procfile and update ports

--- app.js
+++ app.js
@@ -318,6 +318,6 @@ app.post("/signin", function(req, res) {
   });
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server is running on port 3000");
 });
